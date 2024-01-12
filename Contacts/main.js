b1461233eaa5a15a2128d849b9b88152ee3e684b
Pedyz <pdromartins11@gmail.com>
2024-01-12 03:08:31 -0300

    Added contacts in Contacts Page

--- Contacts/main.js
+++ Contacts/main.js
@@ -2,6 +2,8 @@ const homeBtn = document.getElementById('home')
 const aboutBtn = document.getElementById('about-me')
 const projectsBtn = document.getElementById('projects')
 const contactsBtn = document.getElementById('contacts')
+const instaBtn = document.getElementById('insta')
+const githubBtn = document.getElementById('github')
 
 function openHomePage() {
     window.location.href = '../index.html'
@@ -19,6 +21,14 @@ function openContactsBtn() {
     window.location.href = 'index.html'
 }
 
+function openInstagram() {
+    window.open('https://www.instagram.com/_pedsuu', '_blank')
+}
+
+function openGitHub() {
+    window.open('https://www.github.com/pedyz/', '_blank')
+}
+
 homeBtn.addEventListener('click', openHomePage)
 
 aboutBtn.addEventListener('click', openAboutPage)
@@ -26,3 +36,7 @@ aboutBtn.addEventListener('click', openAboutPage)
 projectsBtn.addEventListener('click', openProjectsPage)
 
 contactsBtn.addEventListener('click', openContactsBtn)
+
+instaBtn.addEventListener('click', openInstagram)
+
+githubBtn.addEventListener('click', openGitHub)
